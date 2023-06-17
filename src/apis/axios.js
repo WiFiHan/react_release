9be2508b482c4e1dda6661cf9999091efd873b1d
WiFiHan <mukss0909056@gmail.com>
2023-06-17 17:28:12 +0900

    feat:please

--- src/apis/axios.js
+++ src/apis/axios.js
@@ -12,7 +12,7 @@ if (process.env.NODE_ENV === "development") {
   axios.defaults.baseURL = "http://localhost:8000/api";
 } else {
   axios.defaults.baseURL =
-    "https://port-0-snulion-week12-7xwyjq992llizmajzo.sel4.cloudtype.app/";
+    "https://port-0-snulion-week12-7xwyjq992llizmajzo.sel4.cloudtype.app/api";
 }
 
 // 누구나 접근 가능한 API들
